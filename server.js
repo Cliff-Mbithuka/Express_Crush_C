fda77338af2b3812c2542c6511efa88309be7439
Clifford Musyoka <Clifford Musyoka>
2024-05-10 17:51:54 +0300

    Add file

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
  import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import posts from './routes/posts.js';
 import logger from './middleware/logger.js';
 import errorHandler  from './middleware/error.js';
 import notFound from './middleware/notFound.js';
+import { log } from 'console';
 const port = process.env.PORT || 8000;
 const app = express();
+
+//Get the directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //Body Parser middleware
 app.use(express.json());
@@ -15,7 +21,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(logger);
 
 //setup static folder
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //ROUTES
 app.use('/api/posts', posts);
